d574a9d7f96c8a24453d36240dea4a1496e5cee3
aleexxii <amaleexxii@gmail.com>
2025-09-18 21:24:56 +0530

    added advertisement banner image

--- src/components/advertise/HomePageTopAd.tsx
+++ src/components/advertise/HomePageTopAd.tsx
@@ -2,8 +2,8 @@
 const HomePageTopAd = () => {
   return (
     <div className="flex items-center justify-center">
-    <div className="w-[1280px] h-[280px] bg-slate-100">
-
+    <div className="w-[1280px] h-[280px] bg-slate-100 ">
+        <img src='images/advertisement.jpg' alt="" className="w-full h-full" />
     </div>
     </div>
   )
